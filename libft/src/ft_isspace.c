ea82a49334aef5bd621fc077ca22bb3fbb5db1ae
omiroshn <alexey.miroshnik1@gmail.com>
2018-09-04 14:50:09 +0300

    sum of ants

--- libft/src/ft_isspace.c
+++ libft/src/ft_isspace.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lemin.h                                            :+:      :+:    :+:   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omiroshn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/03 13:21:25 by omiroshn          #+#    #+#             */
-/*   Updated: 2018/09/03 13:21:26 by omiroshn         ###   ########.fr       */
+/*   Created: 2017/10/30 21:09:48 by omiroshn          #+#    #+#             */
+/*   Updated: 2017/10/30 21:09:49 by omiroshn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LS_H
-# define FT_LS_H
+#include "libft.h"
 
-# include "libft.h"
-# include "colored_str.h"
-
-typedef struct		s_lemin
+int		ft_isspace(int c)
 {
-	int		count_ants;
-	int		count_start;
-	int		flag_start;
-	int		count_end;
-	int		flag_end;
-}					t_lemin;
-
-#endif
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || \
+		c == '\r');
+}
--- src/main.c
+++ src/main.c
@@ -12,8 +12,61 @@
 
 #include "lemin.h"
 
+void	print_error()
+{
+	ft_printf("ERROR\n");
+	//system("leaks lemin");
+	exit(-1);
+}
+
+void	check_commands(char *line, t_lemin *lemin)
+{
+	if (ft_strcmp(line, "##start") == 0)
+	{
+		lemin->count_start++;
+		lemin->flag_start = 1;
+	}
+	else if (ft_strcmp(line, "##end") == 0)
+	{
+		lemin->count_end++;
+		lemin->flag_end = 1;
+	}
+	else if (line[0] != '#')
+		print_error();
+	if (lemin->count_start > 1 || lemin->count_end > 1)
+		print_error();
+	//join_str(lemin, line);
+}
+
+void	read_amount_of_ants(t_lemin *lemin)
+{
+	char *line;
+	int i;
+
+	while (get_next_line(0, &line) && line[0] == '#')
+	{
+		if (!ft_strstr(line, "##start") || !ft_strstr(line, "##end"))
+			print_error();
+		check_commands(line, lemin);
+	}
+	i = -1;
+	while (line[++i])
+	{
+		if (ft_isspace(line[i]))
+			print_error();
+		else if (!ft_isdigit(line[i]))
+			print_error();
+	}
+	lemin->count_ants = ft_atoi(line);
+	
+	ft_printf("ants: %d\n", lemin->count_ants);
+}
+
 int		main(int argc, char **argv)
 {
-	ft_printf("lemin");
+	t_lemin *lemin;
+
+	lemin = (t_lemin*)malloc(sizeof(t_lemin));
+	read_amount_of_ants(lemin);
 	return (0);
 }
